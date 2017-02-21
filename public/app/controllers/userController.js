48c1c25909208f72ba19aac49c027d099193167b
starlordvk <chocoshake2007@gmail.com>
2017-02-21 19:13:39 +0530

    Added some comments

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -7,11 +7,13 @@ userController.controller("regControl",function($http){
 	app.successMsg=false;
 	 this.regUser=function(regData){
 		console.log("form submitted");
+		//making an HTTP request to the backend
 		$http.post('/api/users',this.regData).then(function(data){
 
 			console.log(data.data.success);
 			console.log(data.data.message);
 
+			//if user creation is successfull and json data received has success=true
 			if(data.data.success)
 			{
 				//Create success message and redirect to the homepage
